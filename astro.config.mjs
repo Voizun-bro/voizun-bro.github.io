6ece88e622f35a4f361dfeb438f152bd9546b905
Voizun-bro <Voizun-bro@users.noreply.github.com>
2024-11-11 05:19:15 +0000

    Apply automatic changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
-import embed from 'astro-embed';
 
-export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    mdx(),
-    embed(),
-  ],
-});
+// https://astro.build/config
+export default defineConfig({});
