2ccc918c527ad033639ba0acaca931b5c7ddd65a
voizun <83576282+voizun@users.noreply.github.com>
2024-11-11 20:08:55 +0900

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   integrations: [
-    nodePolyfills(),
     tailwind(),
     react(),
     mdx(),
   ],
+  vite: {
+    plugins: [
+      nodePolyfills(),
+    ]
+  }
 });
 
